0423a18d0dab5a559c413b3397f9e742e7070ad5
Bdita <fpbandita@gmail.com>
2017-05-08 00:26:52 +1000

    latest updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,4 @@
 class PagesController < ApplicationController
-  def home
-    
-    #give some data
-    #can access them in controllers view page
-  end
-
   def about
-  end
-
-  def work
-  end
-
-  def blog
-  end
-
-  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#about'
 
-  get 'pages/about'
-
-  get 'pages/work'
-
-  get 'pages/blog'
-
-  get 'pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
